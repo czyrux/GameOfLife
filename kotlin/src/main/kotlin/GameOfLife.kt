b7e77ea9c89ea548af8de85e2b31c0cbde43fb98
czyrux <agutierrez88s@gmail.com>
2019-11-17 20:21:29 +0000

    Create Game Board

--- kotlin/src/main/kotlin/GameOfLife.kt
+++ kotlin/src/main/kotlin/GameOfLife.kt
@@ -1,7 +1,60 @@
-fun main(args : Array<String>) {
+import model.Cell
+import model.CellState
+import model.GameOfLifeBoard
+
+fun main(args: Array<String>) {
     println("Hello, World!")
 
-
+    var board = GameOfLifeBoard.Builder()
+        .withRow(
+            listOf(
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive
+            )
+        )
+        .withRow(
+            listOf(
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Dead,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive
+            )
+        )
+        .withRow(
+            listOf(
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Dead,
+                CellState.Dead,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive
+            )
+        )
+        .withRow(
+            listOf(
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Alive,
+                CellState.Dead
+            )
+        )
+        .build()
 /*    Generation 1:
     4 8
     ........
