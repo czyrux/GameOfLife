edc50bd32ad6afb7a8f2616c651dfd12f3068e7d
czyrux <agutierrez88s@gmail.com>
2019-11-19 00:18:25 +0000

    Fix sample generations 1 and 2 (alive and dead were reverted)

--- kotlin/src/main/kotlin/model/GameOfLifeBoardFactory.kt
+++ kotlin/src/main/kotlin/model/GameOfLifeBoardFactory.kt
@@ -9,53 +9,114 @@ Generation 1:
 ........
 */
 
-fun createSampleBoard() =
+fun createSampleBoardGen1() =
     GameOfLifeBoard.Builder()
         .withRow(
             listOf(
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead
             )
         )
         .withRow(
             listOf(
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
                 Cell.Dead,
                 Cell.Alive,
-                Cell.Alive,
-                Cell.Alive
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead
             )
         )
         .withRow(
             listOf(
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
+                Cell.Dead,
                 Cell.Dead,
                 Cell.Dead,
                 Cell.Alive,
                 Cell.Alive,
-                Cell.Alive
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead
             )
         )
         .withRow(
             listOf(
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Alive
+            )
+        )
+        .build()
+
+/*
+Generation 2:
+4 8
+........
+...**...
+...**...
+........
+*/
+
+fun createSampleBoardGen2() =
+    GameOfLifeBoard.Builder()
+        .withRow(
+            listOf(
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead
+            )
+        )
+        .withRow(
+            listOf(
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
                 Cell.Alive,
                 Cell.Alive,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead
+            )
+        )
+        .withRow(
+            listOf(
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
                 Cell.Alive,
                 Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
-                Cell.Alive,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead
+            )
+        )
+        .withRow(
+            listOf(
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
+                Cell.Dead,
                 Cell.Dead
             )
         )
